3f1bf9e31c39021a9cd5f7236f287c79affe2fb1
e1b20111 <e1b20111@st.oit.ac.jp>
2022-10-24 07:52:32 +0900

    sample4-2 小屋に最初に入室した人の表示

--- src/main/java/oit/is/z0769/kaizi/database/controller/Sample41Controller.java
+++ src/main/java/oit/is/z0769/kaizi/database/controller/Sample41Controller.java
@@ -1,11 +1,14 @@
 package oit.is.z0769.kaizi.database.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import oit.is.z0769.kaizi.database.model.Chamber;
+import oit.is.z0769.kaizi.database.model.ChamberMapper;
 
 
 /**
@@ -15,8 +18,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
+
   @GetMapping("step1")
   public String sample41() {
+    return "sample41.html";
+  }
+
+  /**
+   * 指定したidをPATHパラメータで受け取り，そのidに対応するデータを取得して返す
+   *
+   * @param id
+   * @param model
+   * @return
+   */
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
     return "sample41.html";
   }
 }
