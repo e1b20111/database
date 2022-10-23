d4635125bded2c24c70ae3342069f7c85c992c9d
e1b20111 <e1b20111@st.oit.ac.jp>
2022-10-24 06:38:07 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z0769/kaizi/database/DatabaseApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z0769.kaizi.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z0769/kaizi/database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z0769.kaizi.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
