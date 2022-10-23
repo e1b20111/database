fa61700a56939fc7b14cb6dc3bcb37c8cb9f8332
e1b20111 <e1b20111@st.oit.ac.jp>
2022-10-24 07:58:30 +0900

    sample31.htmlの表示

--- src/main/java/oit/is/z0769/kaizi/database/controller/Sample41Controller.java
+++ src/main/java/oit/is/z0769/kaizi/database/controller/Sample41Controller.java
@@ -1,5 +1,10 @@
 package oit.is.z0769.kaizi.database.controller;
 
+import java.security.Principal;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import oit.is.z0769.kaizi.database.model.Chamber;
 import oit.is.z0769.kaizi.database.model.ChamberMapper;
-
 
 /**
  * /sample4へのリクエストを扱うクラス authenticateの設定をしていれば， /sample4へのアクセスはすべて認証が必要になる
@@ -21,10 +25,14 @@ public class Sample41Controller {
   @Autowired
   ChamberMapper chamberMapper;
 
-
   @GetMapping("step1")
   public String sample41() {
     return "sample41.html";
+  }
+
+  @GetMapping("step3")
+  public String sample43() {
+    return "sample43.html";
   }
 
   /**
@@ -40,5 +48,28 @@ public class Sample41Controller {
     model.addAttribute("chamber2", chamber2);
 
     return "sample41.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   *
+   *         Transactionalはメソッドでトランザクション処理を実施したい場合に付与する
+   *         このメソッドが開始するとトランザクションが開始され，メソッドが正常に終了するとDBへのアクセスが確定する（Runtime
+   *         errorなどで止まった場合はロールバックが行われる）
+   */
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    Chamber chamber3 = new Chamber();
+    chamber3.setChamberName(chamberName);
+    chamber3.setUserName(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    // System.out.println("ID:" + chamber3.getId());
+    return "sample43.html";
   }
 }
